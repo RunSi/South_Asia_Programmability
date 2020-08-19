07ee76c561111f62d21a0b77039ec961f27bf08e
simon <sihart@cisco.com>
2020-08-19 08:00:13 +0100

    assert addition

--- assertexample.py
+++ assertexample.py
@@ -1,3 +1,11 @@
-var = 'a'
+x = "hello"
 
-assert var == 'b'
+assert x == 'goodbye'
+
+# assert x == 'goodbye', 'well that is wrong'
+
+# try:
+#     assert x == 'goodbye'
+#     print('\nx is goodbye\n')
+# except:
+#     print('\nx is not goodbye\n')
